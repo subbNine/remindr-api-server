dec326b401e7514d724020fb33c56098711cea16
Prince Ita <princeIta@outlook.com>
2025-06-28 06:18:31 +0100

    update swagger path

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+  SwaggerModule.setup("api-docs", app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
